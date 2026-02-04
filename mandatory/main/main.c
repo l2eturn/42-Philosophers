75e28bf834fb89d0850c6477846460b7435f166f
root <root@Lenovo16.localdomain>
2026-02-04 16:12:54 +0700

    added

--- mandatory/main/main.c
+++ mandatory/main/main.c
@@ -12,16 +12,28 @@
 
 #include "philosopher.h"
 #include "utils.h"
+#include <limits.h>
 
 // input must take only 5,6 arg inlucde ./a.out
 // ./philo number_of_philos time_die time_eat time_sleep (extra_for_time_to_eat_each_philos)
 
 void	valid_input(int ac, char **av)
 {
+	int	i;
+
+	i = 1;
 	if ((ac != 5) && (ac != 6)) 
 		error_message("Invalid Input Format\n", 1);
-	// if ()
-	// 	error_message("Invalid Input Format\n", 1);
+	while (i ++)
+	{
+
+
+
+
+	}
+	if ()
+		error_message("Invalid Input Format\n", 1);
+		
 }
 
 int main(int ac, char **av)
@@ -31,7 +43,6 @@ int main(int ac, char **av)
 	t_shared			shared;
 	int					numb_philos;
 
-	printf("%d\n", ac);
 	valid_input(ac, av);
 	// numb_philos = (int)ft_atoi(av[1]);
 	// philos = malloc(sizeof(pthread_t) * numb_philos);
